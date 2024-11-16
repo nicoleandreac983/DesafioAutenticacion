51fa0e19c5e9c0ad08d48a25dd802ea423f39980
nicoleandreac983 <nicole_030790@hotmail.com>
2024-11-15 21:45:41 -0300

    descartando variables de entorno no funciona

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VUE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VUE_FIREBASE_AUTH_DOMAIN, 
-  projectId: import.meta.env.VUE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VUE_FIREBAE_STORAGEB,
-  messagingSenderId: import.meta.env.VUE_FIREBAE_MESSAGEIN,
-  appId: import.meta.env.VUE_FIREBASE_APPI_ID
+  apiKey:"AIzaSyAdvZZ4C1Qif0BieagAO_00VJqe-6vKwtc", //import.meta.env.VUE_FIREBASE_API_KEY,
+  authDomain: "desafioautentificacion-d67e0.firebaseapp.com", //import.meta.en.VUE_FIREBASE_AUTH_DOMAIN, 
+  projectId: "desafioautentificacion-d67e0", //import.meta.env.VUE_FIREBASE_PROJECTID,
+  storageBucket: "desafioautentificacion-d67e0.firebasestorage.app", //import.meta.env.VUE_FIREBAE_STORAGEB,
+  messagingSenderId: "447027999710", //import.meta.env.VUE_FIREBAE_MESSAGEIN,
+  appId:"1:447027999710:web:cdbd18904b8c4d1985e34f", //import.meta.env.VUE_FIREBASE_APPI_ID
 };
 
 // Initialize Firebase
