743e3e41cfcb48183d2b2446979ed3272528fe88
nicoleandreac983 <nicole_030790@hotmail.com>
2024-11-15 21:03:21 -0300

    mas variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VUE_FIREBASE_API_KEY,
-  authDomain: "desafioautentificacion-d67e0.firebaseapp.com",
-  projectId: "desafioautentificacion-d67e0",
-  storageBucket: "desafioautentificacion-d67e0.firebasestorage.app",
-  messagingSenderId: "447027999710",
-  appId: "1:447027999710:web:cdbd18904b8c4d1985e34f"
+  authDomain: import.meta.en.VUE_FIREBASE_AUTH_DOMAIN, 
+  projectId: import.meta.env.VUE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VUE_FIREBAE_STORAGEB,
+  messagingSenderId: import.meta.env.VUE_FIREBAE_MESSAGEIN,
+  appId: import.meta.env.VUE_FIREBASE_APPI_ID
 };
 
 // Initialize Firebase
